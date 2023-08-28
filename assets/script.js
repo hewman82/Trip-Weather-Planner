6bcac85ba374db544f26be4e8a67a242a2247d29
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-08-28 15:39:09 -0700

    added function to display current weather of selected city

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+var currWeatherCard = document.querySelector('#current-weather');
+var currWeatherHead = document.querySelector('#current-date');
+var currTempText = document.querySelector('#current-temp');
+var currWindText = document.querySelector('#current-wind');
+var currHumText = document.querySelector('#current-humidity');
 var searchCard = document.querySelector('#search');
 var searchBtn = document.querySelector('#search-btn');
 var searchBar = document.querySelector('#search-bar');
@@ -12,15 +17,15 @@ searchBtn.addEventListener('click', function(e) {
 });
 
 function getCoordinates(searchText) {
-    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchText + '&limit=5&appid=7d6d7f71cbdd0e76a6f7fb3306fcce7f';
+    var geocodeAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchText + '&limit=5&appid=7d6d7f71cbdd0e76a6f7fb3306fcce7f';
 
-    fetch(apiUrl)
+    fetch(geocodeAPI)
     .then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
                 displayCities(data);
             });
-        } else alert(response.status);
+        } else { alert(response.status); }
     });
 
 }
@@ -42,11 +47,52 @@ function displayCities(data) {
 }
 
 optionList.addEventListener('click', function selectCity(event) {
-    console.log(event.target);
     var selected = event.target;
     var lat = selected.getAttribute('data-lat');
     var lon = selected.getAttribute('data-lon');
-
-
+    getWeather(lat,lon);
+    /*getForecast(lat, lon);*/
 
 });
+
+function getWeather(lat, lon) {
+    var currentWeatherAPI = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=7d6d7f71cbdd0e76a6f7fb3306fcce7f&units=imperial';
+
+    fetch(currentWeatherAPI)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayCurrentWeather(data);
+            });
+        } else { alert(response.status); }
+        
+    });
+}
+
+function displayCurrentWeather(data) {
+    console.log(data);
+    var name = data.name;
+    var currDate = dayjs.unix(data.dt).format(' (M, DD, YYYY)');
+    var currTemp = data.main.temp;
+    var currWind = data.wind.speed;
+    var currHum = data.main.humidity;
+
+    currWeatherHead.textContent = name + currDate;
+    currTempText.textContent = 'Temperature: ' + currTemp + '°F';
+    currWindText.textContent = 'Wind Speed: ' + currWind + ' mi/hr';
+    currHumText.textContent = 'Humidity: ' + currHum + '%';
+}
+
+function getForecast(lat, lon) {
+    var weatherForecastAPI = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=7d6d7f71cbdd0e76a6f7fb3306fcce7f&units=imperial';
+
+    fetch(weatherForecastAPI)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        } else { alert(response.status); }
+        
+    });
+}
